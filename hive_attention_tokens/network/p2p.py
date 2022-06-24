63b8cc12e940a95cb1b7ad782d16a364baf6df8f
imwatsi <imwatsi@gmail.com>
2022-06-24 18:33:43 +0400

    init

---
+++ hive_attention_tokens/network/p2p.py
@@ -0,0 +1,13 @@
+class Peer:
+    # load info
+    # check status
+    pass
+
+class PeerToPeer:
+    # start
+    # discovery
+    # sync state
+    # broadcast messages
+    # direct messages
+    pass
+
